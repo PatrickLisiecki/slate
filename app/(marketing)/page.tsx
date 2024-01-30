7a08763f3e5e140de8e9423564306b370d85b803
PatrickLisiecki <impatricklisiecki@gmail.com>
2024-01-30 17:01:22 -0500

    fix padding

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -5,7 +5,7 @@ import Footer from "./_components/footer";
 export default function MarketingPage() {
   return (
     <div className="flex min-h-full flex-col">
-      <div className="md::justify-start flex flex-1 flex-col items-center justify-center gap-y-8 px-6 pb-10 text-center">
+      <div className="md::justify-start flex flex-1 flex-col items-center justify-center gap-y-8 px-6 text-center">
         <Heading />
         <Heroes />
       </div>
